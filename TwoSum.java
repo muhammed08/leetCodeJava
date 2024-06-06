92959ed73e3dfd6927213d13da888bd62c082b3b
muhammed08 <muhammedameen08@gmail.com>
2024-06-07 00:23:49 +0530

    Sum of numbers represented as linked list

--- TwoSum.java
+++ TwoSum.java
@@ -1,8 +1,11 @@
-"""
+/*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-"""
+*/
+import java.util.HashMap;
+import java.util.Map;
+
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> listMap = new HashMap();
